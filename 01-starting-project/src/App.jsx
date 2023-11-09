3be84f5ca77a585f8555163ec84f6bfe0a6db0ff
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-11-09 10:11:36 -0700

    Add more content using state

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,9 +3,10 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from "./data.js"
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please Click a Button')
+  const [ selectedTopic, setSelectedTopic ] = useState('components')
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -28,12 +29,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Documents</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
